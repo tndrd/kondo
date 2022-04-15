0f049d31f312f42361b88727da4954f283eeae85
tndrd <vovalekhterev@gmail.com>
2022-04-15 15:21:08 +0300

    Create kondo workspace

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/tndrd/workspaces/kondo/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/tndrd/workspaces/kondo/devel;/opt/ros/noetic'.split(';') if '/home/tndrd/workspaces/kondo/devel;/opt/ros/noetic' != '' else []
---
+++ src/mytest/scripts/test.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import Float64
+
+import controller
+print(dir(controller))
+
+from controller import Robot
+
+
+print("hehe")
+
+robot = Robot()
+timeStep = int(robot.getBasicTimeStep())
+
+head_yaw = robot.getDevice('head_yaw')
+
+head_yaw.setPosition(float('inf'))
+
+velocity = 1
+
+head_yaw.setVelocity(velocity)
+
+while robot.step(timeStep) != -1 and not rospy.is_shutdown():
+    rospy.spinOnce()
